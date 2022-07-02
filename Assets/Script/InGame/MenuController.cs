b075ccdd6e55bf556d49c5e9a4a4a83779e59269
sangmin-ra <sangmin.ra@nts-corp.com>
2022-07-02 18:08:50 +0900

    상점 팝업 구현

--- Assets/Script/InGame/MenuController.cs
+++ Assets/Script/InGame/MenuController.cs
@@ -1,4 +1,4 @@
-using System.Collections ;
+﻿using System.Collections ;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +7,7 @@ public class MenuController : MonoBehaviour
     int menuPanelStatus = 0;
 
     [SerializeField] List<RectTransform> menuPanelList = new List<RectTransform>();
+    [SerializeField] RectTransform popupParant;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +23,34 @@ public class MenuController : MonoBehaviour
 
     public void MenuBtnClick(int index)
     {
-        Debug.LogWarning(index);
+        CreatePopup(index);
     }
+
+    void CreatePopup(int index) 
+    {
+        string popupName = string.Empty;
+
+        switch (index)
+        {
+            case 0: popupName = "ShopPopup";
+                break;
+        }
+
+        BasePopup basePopup = null;
+
+        if (string.IsNullOrEmpty(popupName) == false)
+        {
+            basePopup = Instantiate(Resources.Load<BasePopup>("UI/Popup/" + popupName), popupParant);
+            basePopup.Init();
+        }
+
+        if (basePopup != null )
+        {
+            Debug.LogWarning("creat 성공");
+        }
+        
+    }
+
     public void MenuPanelChangeOn(int status)
     {
         menuPanelStatus = status;
